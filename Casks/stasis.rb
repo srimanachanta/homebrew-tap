3fa665fc6037ca63e49fbddc0f0146c799a8d220
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2026-02-25 03:25:37 -0500

    Bump Stasis version to 0.2.0

--- Casks/stasis.rb
+++ Casks/stasis.rb
@@ -1,5 +1,5 @@
 cask "stasis" do
-  version "0.1.0"
+  version "0.2.0"
   sha256 "d1e984ff923ce24b96b121e1d771d6c7c506a622872396656305c9b8c2ecc105"
 
   url "https://github.com/srimanachanta/Stasis/releases/download/v#{version}/Stasis.dmg"
