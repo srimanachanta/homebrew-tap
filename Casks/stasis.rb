7ffc114ce4d8b3c4b41d65e2b76dba898d37c89e
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2026-02-25 03:26:01 -0500

    Update SHA256 checksum for Stasis cask
    Updated SHA256 checksum for version 0.2.0.

--- Casks/stasis.rb
+++ Casks/stasis.rb
@@ -1,6 +1,6 @@
 cask "stasis" do
   version "0.2.0"
-  sha256 "d1e984ff923ce24b96b121e1d771d6c7c506a622872396656305c9b8c2ecc105"
+  sha256 "6bc1829af17303cedbc4ee1ac1a115bffeec7b9770831f2f5e5ebd6eb597aab0"
 
   url "https://github.com/srimanachanta/Stasis/releases/download/v#{version}/Stasis.dmg"
   name "Stasis"
